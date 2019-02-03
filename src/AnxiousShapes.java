3b0a6c217cd488b3acc4f44ac148e91a5a03bd21
Raghu Khanal <Khanalraghu03@gmail.com>
2019-02-03 17:39:43 -0500

    Made some shapes

--- src/AnxiousShapes.java
+++ src/AnxiousShapes.java
@@ -1,176 +1,174 @@
+/**Class: AnxiousShapes
+ * @author Raghu Khanal
+ * @version 1.0
+ * Course : ITEC 3150
+ * Written: February 3, 2019
+ *
+ *
+ * This class is a very simple shape with movement associated with it if you
+ * utilize the calculateTt method
+ *
+ */
+
 import javafx.animation.TranslateTransition;
-import javafx.application.Application;
 import javafx.collections.ObservableList;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
-import javafx.stage.Stage;
 import javafx.util.Duration;
-
 import java.util.Random;
 
-public class AnxiousShapes extends Application {
+public class AnxiousShapes {
+
     private Shape shape;
-    private Random rand = new Random();
-    private int size;
     private double delay;
     private int howFarToMoveX;
     private int howFarToMoveY;
+    private Random rand = new Random();
     private int x;
     private int y;
-
+    private Color color;
+    private int size;
 
     public AnxiousShapes(int type, int x, int y, int size, double delay,
                         Color color, int hftmX, int hftmY) {
         this.delay = delay;
         this.x = x;
         this.y = y;
+        this.color = color;
+        this.size = size;
 
-        //Make shapes:
+        // Make shapes:
         if (type % 4 == 0) {
-            shape = new Circle(x, y, size, color);
+            shape = new Circle(getX(), getX(), getSize(), getColor());
         } else if (type % 4 == 1) {
-            //how to create triangle
+            // how to create triangle
             shape = new Polygon();
             ObservableList<Double> list = ((Polygon) shape).getPoints();
             for (int i = 0; i < 3; i++) {
-                list.add(x + size * Math.cos(2 * i * Math.PI / 3));
-                list.add(y - size * Math.sin(2 * i * Math.PI / 3));
+                list.add(getX() + getSize() * Math.cos(2 * i * Math.PI / 3));
+                list.add(getY() - getSize() * Math.sin(2 * i * Math.PI / 3));
             }
-            shape.setFill(color);
+            shape.setFill(getColor());
 
         } else if (type % 4 == 2) {
-//              //how to create rectangle
-//               shape = new Rectangle(x, y, width, height);
+            // //how to create rectangle
+            shape = new Rectangle(getX(), getY(), getWidth(), getHeight());
+            shape.setFill(getColor());
         } else if (type % 4 == 3) {
-//              //how to create pentagon
-                 shape = new Polygon();
+            // //how to create pentagon
+            shape = new Polygon();
             ObservableList<Double> list = ((Polygon) shape).getPoints();
             for (int i = 0; i < 6; i++) {
-               list.add(x + size * Math.cos(2 * i * Math.PI / 6));
-               list.add(y - size * Math.sin(2 * i * Math.PI / 6));
+                list.add(getX() + getSize() * Math.cos(2 * i * Math.PI / 6));
+                list.add(getY() - getSize() * Math.sin(2 * i * Math.PI / 6));
             }
-            shape.setFill(color);
+            shape.setFill(getColor());
 
         } else {
             System.out.println("Something went wrong!");
         }
 
+        this.delay = delay;
+        this.howFarToMoveX = hftmX;
+        this.howFarToMoveY = hftmY;
+    }
+    // ThreadLocalRandom.current().nextInt();
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    public double getDelay() {
+        return delay;
+    }
+
+    public void setDelay(double delay) {
+        this.delay = delay;
+    }
+
+    public int getHowFarToMoveX() {
+        return howFarToMoveX;
+    }
+
+    public void setHowFarToMoveX(int howFarToMoveX) {
+        this.howFarToMoveX = howFarToMoveX;
+    }
+
+    public int getHowFarToMoveY() {
+        return howFarToMoveY;
+    }
+
+    public void setHowFarToMoveY(int howFarToMoveY) {
+        this.howFarToMoveY = howFarToMoveY;
+    }
+
+    public int getWidth() {
+        // will generate the value of x btwn: 10-50
+
+        return rand.nextInt(50 - 10) + 10;
+    }
+    public int getHeight() {
+        // will generate the value of y btwn: 25-100
+        return rand.nextInt(100 - 25) + 25;
+    }
+
+    public TranslateTransition calculateTt() {
+        TranslateTransition tt = new TranslateTransition(
+                Duration.seconds(delay), shape);
+        tt.setToX(shape.getLayoutX() + howFarToMoveX);
+        tt.setToY(shape.getLayoutY() + howFarToMoveY);
+        // Let the animation run forever -- if the shape
+        // tries to move "off-screen" it will return to the beginning
+        tt.setCycleCount(TranslateTransition.INDEFINITE);
+        return tt;
+    }
+
+    // another getter and setter
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Pane pane = new Pane();
-        Shape rectangle = drawRectangle();
-        Shape circle = drawCircle();
-
-        pane.getChildren().add(rectangle);
-        pane.getChildren().add(circle);
-
-        Scene scene = new Scene(pane, getScreenSize(), getScreenSize());
-
-        primaryStage.setTitle("Anxious Shape");
-
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public String toString() {
+        return "AnxiousShape [shape=" + shape + ", delay=" + delay
+                + ", howFarToMoveX=" + howFarToMoveX + ", howFarToMoveY="
+                + howFarToMoveY + "]";
     }
-
-//     ThreadLocalRandom.current().nextInt();
-
-//    public Shape getShape() {
-//        return shape;
-//    }
-//
-//    public void setShape(Shape shape) {
-//        this.shape = shape;
-//    }
-//
-//    public double getDelay() {
-//        return delay;
-//    }
-//
-//    public void setDelay(double delay) {
-//        this.delay = delay;
-//    }
-//
-//    public int getHowFarToMoveX() {
-//        return howFarToMoveX;
-//    }
-//
-//    public void setHowFarToMoveX(int howFarToMoveX) {
-//        this.howFarToMoveX = howFarToMoveX;
-//    }
-//
-//    public int getHowFarToMoveY() {
-//        return howFarToMoveY;
-//    }
-//
-//    public void setHowFarToMoveY(int howFarToMoveY) {
-//        this.howFarToMoveY = howFarToMoveY;
-//    }
-//
-//    public TranslateTransition calculateTt() {
-//        TranslateTransition tt = new TranslateTransition(
-//                Duration.seconds(delay), shape);
-//        tt.setToX(shape.getLayoutX() + howFarToMoveX);
-//        tt.setToY(shape.getLayoutY() + howFarToMoveY);
-//        // Let the animation run forever -- if the shape
-//        // tries to move "off-screen" it will return to the beginning
-//        tt.setCycleCount(TranslateTransition.INDEFINITE);
-//        return tt;
-//    }
-//
-    public int generateWidth() {
-        //We will generate the value of x btwn: 10-50
-
-        return rand.nextInt(50-10) +10;
-    }
-    public int generateHeight() {
-        //We will generate the value of y btwn: 25-100
-        return rand.nextInt(100-25) +25;
-    }
-
-    public int getScreenSize() {
-        return rand.nextInt(750-500)+500;
-    }
-//
-//    public int getNumOfShapes() {
-//        return rand.nextInt(200) + 100;
-//    }
-//    public Color getColor() {
-//        return new Color(Math.random(), Math.random(), Math.random(), Math.random());
-//    }
-    public int generatePositionX(){
-        return rand.nextInt(getScreenSize());
-    }
-    public int generatePositionY(){
-        return rand.nextInt(getScreenSize());
-    }
-
-    public Rectangle drawRectangle() {
-        return new Rectangle(generatePositionX(), generatePositionY(),generateWidth() , generateHeight());
-    }
-    public Circle drawCircle() {
-        return new Circle(generatePositionX(), generatePositionY(), 25 );
-    }
-    public Polygon drawPentagon() {
-        return new Polygon();
-    }
-    public Polygon drawTriangle() {
-        return new Polygon();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-
 
 }
--- src/AnxiousShapesTester.java
+++ src/AnxiousShapesTester.java
@@ -1,62 +1,82 @@
-import javafx.collections.ObservableList;
+/**Class: AnxiousShapesTester
+ * @author Raghu Khanal
+ * @version 1.0
+ * Course : ITEC 3150
+ * Written: February 3, 2019
+ *
+ *
+ * This class is a tester class for AnxiousShapes and generates bunch of shapes with the help
+ *  of the AnxiousShapes.java class. It add them to the GUI.
+ *
+ */
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.util.Random;
-import static javafx.application.Application.launch;
-//import javafx.scene.paint.Color;
 
-public class AnxiousShapesTester {
+
+public class AnxiousShapesTester extends Application {
+
     public static void main(String[] args) {
-
-        Random rand = new Random();
-        int type = 0;
-        int screenSize = rand.nextInt(750)+500;
-        int x = rand.nextInt(screenSize);
-        int y = rand.nextInt(screenSize);
-        int size = 67;
-        int delay = rand.nextInt(25) + 10;
-      Color color = new Color(Math.random(), Math.random(), Math.random(), Math.random());
-        int hftmX = 3;
-        int hftmY = 2;
-//    int numOfShapes = rand.nextInt(200) + 100;
-        //randomly generate these
-
-
-
-//        int maxSize = rand.nextInt(100) + 50;
-//        int minSize = rand.nextInt(25) + 10;
-//        int windowSize = rand.nextInt(750) + 500;
-        AnxiousShapes aShapes = null;
-        Pane pane = new Pane();
-//        ArrayList<AnxiousShapes> list = new ArrayList<>();
-//        while(type < numOfShapes) {
-             aShapes = new AnxiousShapes(type, x, y, size, delay,
-                    color, hftmX, hftmY);
-////            list.add(aShapes);
-//
-//            type++;
-//        }
-
-
-        // will have different anxious shapes every time because I am using a Random
-        // class.
-
-        // int type, int x, int y, int size, double delay, Color color, int hftmX, int
-        // hftmY
-        pane.getChildren().add(aShapes.getShape());
-        Scene scene = new Scene(pane, 400, 400);
-
-        Stage stage = new Stage();
-        stage.setScene();
-        aShapes.start(stage);
-
-
-
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Random rand = new Random();
+        // int type = 0;
+        int windowSize = rand.nextInt(750 - 500) + 500;
+        int x;
+        int y;
+        int size;
+        int delay;
+        Color color;
+        int hftmX = 3;
+        int hftmY = 2;
+        int numOfShapes = rand.nextInt(200 - 100) + 100;
+
+        // private int maxSize;
+        // private int minSize;
+        // private int numOfShape;
+        // private int windowSize;
+
+        // int type, int x, int y, int size, double delay, Color color, int
+        // hftmX, int
+        // hftmY
+        // int num = 5;
+        ArrayList asList = new ArrayList();
+        for (int type = 0; type < numOfShapes; type++) {
+
+            x = rand.nextInt(windowSize);
+            y = rand.nextInt(windowSize);
+            size = rand.nextInt(100 - 10) + 10;
+            delay = rand.nextInt(25 - 10) + 10;
+            color = new Color(Math.random(), Math.random(), Math.random(),
+                    Math.random());
+
+            AnxiousShapes as = new AnxiousShapes(type, x, y, size, delay, color,
+                    hftmX, hftmY);
+            as.calculateTt();
+            asList.add(as.getShape());
+        }
+//        System.out.println(numOfShapes + " " + windowSize);
+
+
+        Pane pane = new Pane();
+
+        for (int i = 0; i < asList.size(); i++) {
+            pane.getChildren().add((Node) asList.get(i));
+        }
+
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Nervous Shapes");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
 }
