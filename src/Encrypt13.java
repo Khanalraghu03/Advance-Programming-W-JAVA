02b1c30df943a95b8589f0fb3090e252d4f81723
Raghu Khanal <Khanalraghu03@gmail.com>
2019-02-06 22:20:07 -0500

    Added some more classes

---
+++ src/Encrypt13.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**Class: Encrypt13
+ * @author Raghu Khanal
+ * @version 1.0
+ * Course : ITEC 3150
+ * Written: February 3, 2019
+ *
+ *
+ * This class encrypt a message using a rotation technique
+ *
+ */
+public class Encrypt13 extends Application {
+    //replace each letter in the original message
+    //  by the letter that comes 13 places after
+    //  it in the alphabet.
+    //Use ASCII to add the char + 13
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+
+
+    }
+
+
+}
